e0f1a99cf0476626de04e8dc2f4abd9c48d243ae
Anthony Ho <ahomnky11@gmail.com>
2023-07-22 19:41:19 +0100

    google analytics

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,10 +8,25 @@ import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
 import Testimonials from "@/components/testimonials";
 import Newsletter from "@/components/newsletter";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-QDFB9JD0B8"
+      ></Script>
+      <Script>
+        {`
+  declare var dataLayer: any[];
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QDFB9JD0B8');
+`}
+      </Script>
       <Hero />
     </>
   );
