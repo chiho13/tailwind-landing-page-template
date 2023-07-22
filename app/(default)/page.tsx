37e5f8b50524dd055ad34b66106c673933baaa35
Anthony Ho <ahomnky11@gmail.com>
2023-07-22 20:33:44 +0100

    ga

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -13,20 +13,6 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-QDFB9JD0B8"
-      ></Script>
-      <Script>
-        {`
-  declare var dataLayer: any[];
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-QDFB9JD0B8');
-`}
-      </Script>
       <Hero />
     </>
   );
