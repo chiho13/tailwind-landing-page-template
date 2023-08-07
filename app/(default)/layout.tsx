2598338155ce6b12fad0185e031ffebd5aa36f2d
Anthony Ho <ahomnky11@gmail.com>
2023-08-07 13:11:53 +0100

    cookies

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -6,6 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Footer from "@/components/ui/footer";
+import CookieConsent from "react-cookie-consent";
 
 export default function DefaultLayout({
   children,
@@ -24,6 +25,30 @@ export default function DefaultLayout({
   return (
     <>
       <main className="grow">{children}</main>
+
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        cookieName="mySiteCookies"
+        style={{
+          background: "#2b2b2b",
+          color: "#ffffff",
+          fontFamily: "Arial, sans-serif",
+          fontSize: "16px",
+        }}
+        buttonStyle={{
+          color: "#2b2b2b",
+          fontSize: "16px",
+          backgroundColor: "#f1f1f1",
+          borderRadius: "6px",
+          padding: "2px 10px",
+          fontWeight: "bold",
+        }}
+        expires={150}
+      >
+        We use cookies to provide the best experience on our website. Please
+        accept our cookies for optimal performance.
+      </CookieConsent>
 
       <Footer />
     </>
