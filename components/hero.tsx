3ef7d60284634959bbf87526a54d69689002285c
Anthony Ho <ahomnky11@gmail.com>
2023-08-16 02:24:35 +0100

    pt

--- components/hero.tsx
+++ components/hero.tsx
@@ -73,7 +73,7 @@ export default function Hero() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Hero content */}
 
-        <div className=" pt-32 pb-12 md:pt-40 md:pb-20">
+        <div className=" pt-24 pb-12 md:pt-32 md:pb-20">
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16 h-[900px] ">
             <h1
