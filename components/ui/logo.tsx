3d9978c537316d09d2be9fd6c451203a15879183
Anthony Ho <ahomnky11@gmail.com>
2023-09-12 17:13:28 +0100

    logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
       <Image
-        src="/images/vontane.svg"
+        src="/images/vontane2.svg"
         width={220}
         height={80}
         alt="vontane logo"
